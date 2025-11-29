6ac44e8d99ebc252e59725ea64517c26863191a3
Mohit <77279663+MohitNikhade@users.noreply.github.com>
2025-11-29 21:58:37 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -14,7 +14,7 @@ logging.basicConfig(level=logging.INFO)
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_USER = os.environ.get('EMAIL_USER')
+EMAIL_USER = os.environ.get('EMAIL_USER_NAME')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
 # Email validation regex
